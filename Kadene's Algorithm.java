5940210a08176d04a827ba75a8b263f196ea5316
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2022-09-06 10:36:09 +0530

    Add files via upload

---
+++ Kadene's Algorithm.java
@@ -0,0 +1,38 @@
+//KADENCE ALGORITHM
+//TIME COMPLEXITY: O(n) i.e. Linear time complexity
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n =sc.nextInt();
+		int[]a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    a[i]=sc.nextInt();
+		}
+		int ans=largestSum(a);
+		System.out.println(ans);
+	}
+	public static int largestSum(int[]a)
+	{
+	    int currentmax=a[0];
+	    int globalmax=a[0];
+	    int n =a.length;
+	    for(int i=1;i<n;i++)
+	    {
+	        if(currentmax<0)
+	        {
+	            currentmax=a[i];
+	        }
+	        else{
+	            currentmax+=a[i];
+	        }
+	    if(currentmax>globalmax)
+	    {
+	        globalmax=currentmax;
+	    }
+	    }
+	    return globalmax;
+	}
+}
